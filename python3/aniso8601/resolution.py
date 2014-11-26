f188f2eb81c1310b9862b435a492b4ce6d0fac2d
Ximo Guanter Gonzalbez <ximo@tid.es>
2014-11-26 08:20:49 +0100

    Remove use of enum in Python3

--- python3/aniso8601/resolution.py
+++ python3/aniso8601/resolution.py
@@ -3,10 +3,8 @@
 # This software may be modified and distributed under the terms
 # of the BSD license.  See the LICENSE file for details.
 
-from enum import Enum
+class DateResolution(object):
+    Year, Month, Week, Weekday, Day, Ordinal = list(range(6))
 
-class DateResolution(Enum):
-    Year, Month, Week, Weekday, Day, Ordinal = range(6)
-
-class TimeResolution(Enum):
-    Seconds, Minutes, Hours = range(3)
+class TimeResolution(object):
+    Seconds, Minutes, Hours = list(range(3))
