0bd3439b5e9f63a1ad186b9c9186ddf60fb3585a
Josh Friend <jfriend@michiganlabs.com>
2014-12-17 22:57:38 -0500

    fix compat.py header

--- aniso8601/compat.py
+++ aniso8601/compat.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2014, Brandon Nielsen
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the BSD license.  See the LICENSE file for details.
 
 import sys
 
