c948bd7f6efc32bd33604defb53d4cac41f3ba79
Brandon Nielsen <nielsenb@jetfuse.net>
2015-10-19 15:26:44 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ readme.close()
 
 setup(
     name='aniso8601',
-    version='1.0.1dev',
+    version='1.1.0dev',
     description='A library for parsing ISO 8601 strings.',
     long_description=README_TEXT,
     author='Brandon Nielsen',
