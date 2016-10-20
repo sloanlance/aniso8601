234a91798d2603f5eb9eacacd9c316c332e77511
Brandon Nielsen <nielsenb@jetfuse.net>
2016-10-20 14:15:05 -0500

    Update copyright date.

--- aniso8601/resolution.py
+++ aniso8601/resolution.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2016, Brandon Nielsen
+# All rights reserved.
+#
 # This software may be modified and distributed under the terms
 # of the BSD license.  See the LICENSE file for details.
 
